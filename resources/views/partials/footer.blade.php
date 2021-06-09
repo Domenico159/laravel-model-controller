75c07325a4ef9aee343343c57a0dfe260a9b04b8
Domenico <you@example.com>
2021-06-09 16:46:59 +0200

    Project ultimate

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 {{-- Footer --}}
 
-<footer>
-    footer here
+<footer class="d-flex justify-content-center m-3">
+    Created by Domenico Zummo class#30 &hearts; Boolean Careers &hearts;
 </footer>
